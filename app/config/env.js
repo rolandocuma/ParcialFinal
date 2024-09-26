406dba51f2c993cbd649bb03fb6f37a4a4836c21
rolandocuma <rcumas@miumg.edu.gt>
2024-09-25 19:43:06 -0600

    s

--- app/config/env.js
+++ app/config/env.js
@@ -2,7 +2,7 @@ const env = {
     database: 'umg23420024',
     username: 'umg23420024_user',
     password: '9uk2kAMI7YfWyLAtl7jXCIUwSXgRAfuU',
-    host: 'dpg-crq8ft88fa8c738uhcrg-a.oregon-postgres.render.com',
+    host: 'dpg-crq8ft88fa8c738uhcrg-a',
     dialect: 'postgres',  
     pool: {
       max: 5,
